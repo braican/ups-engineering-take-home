3f0092bedab1e44ff4ccca7a4908b3c4aa6b03ad
Nick Braica <nick.braica@gmail.com>
2019-09-05 10:03:12 -0700

    Set up styles with scss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header>
+      <h1 class="headline">
+        UpDos
+      </h1>
+    </header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// Import the global css.
+import '@/styles/globals.scss';
+
+import trello from '@/services/Trello';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      info: null,
+    };
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 2vw;
 }
 </style>
